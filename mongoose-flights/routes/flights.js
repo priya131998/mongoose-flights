7b95fad683da02a7862214a8d6601daa3f1f73fa
priya131998 <priya.magiya@gmail.com>
2021-07-07 15:05:41 -0700

    Consist all the files

---
+++ mongoose-flights/routes/flights.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// GET /flights/new
+router.get('/', flightsCtrl.index);
+router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
+
+module.exports = router;
